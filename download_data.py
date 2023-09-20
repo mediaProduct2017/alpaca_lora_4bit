3822fde92e404000b4de968fed31d4cdd774f980
arfu <arfu.guo@gmail.com>
2023-09-20 09:37:21 +0800

    inference3.py

--- download_data.py
+++ download_data.py
@@ -7,7 +7,9 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 # DATA = "/content/ai/data"
 DATA = os.path.join(project_root, "ai/data")
 
-os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://raw.githubusercontent.com/mediaProduct2017/blog_english/main/info_extract/qa_train3.json -d {DATA} -o alpaca_data_cleaned_archive.json")
+os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://raw.githubusercontent.com/mediaProduct2017/blog_english/main/info_extract/qa_test3.json -d {DATA} -o alpaca_data_cleaned_archive.json")
+
+# os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://raw.githubusercontent.com/mediaProduct2017/blog_english/main/info_extract/qa_train3.json -d {DATA} -o alpaca_data_cleaned_archive.json")
 
 # os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://raw.githubusercontent.com/mediaProduct2017/blog_english/main/info_extract/qa_train.json -d {DATA} -o alpaca_data_cleaned_archive.json")
 
